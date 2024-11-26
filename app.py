e57e49799b5998b3e95769d4208a21c7afe56cbd
SitiSarah25 <stsarah253@gmail.com>
2024-11-26 19:37:56 +0700

    Menambahkan menu tentang kami

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
