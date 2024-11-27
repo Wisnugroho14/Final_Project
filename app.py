aecbdd526c18c8b10eb01b9131ba2e3b5a5e3f33
Wisnu Nugroho <114457873+Wisnugroho14@users.noreply.github.com>
2024-11-27 13:30:28 +0700

    Mengedit halaman app, style

--- app.py
+++ app.py
@@ -9,22 +9,23 @@ client = MongoClient("mongodb+srv://test:sparta@cluster0.kbfqt.mongodb.net/?retr
 
 # Halaman Utama: 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+# Halaman Contact:    
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
-
 
 # Halaman Login: 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-
 # Halaman Register: 
 @app.route('/register')
 def register():
     return render_template('register.html')
-
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
