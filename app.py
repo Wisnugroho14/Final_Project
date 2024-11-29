aa9214457525aa28603db97872af8db87374383a
wisnugroho14 <wisnuenunugroho@gmail.com>
2024-11-29 20:03:52 +0700

    Menambahkan tampilan halaman  admin

--- app.py
+++ app.py
@@ -37,5 +37,20 @@ def login():
 def register():
     return render_template('register.html')
 
+# Halaman Admin: 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+# Halaman admin user:
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+# Halaman admin program: 
+@app.route('/program-admin')
+def programAdmin():
+    return render_template('program-admin.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
