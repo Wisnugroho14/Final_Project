5c6c99e88a22b17d727d9a363e21c4acc3f2e331
wisnugroho14 <wisnuenunugroho@gmail.com>
2024-12-05 13:30:27 +0700

    membuat menu pendaftaran dengan isi data dummy

--- app.py
+++ app.py
@@ -371,5 +371,10 @@ def delete_user(user_id):
         flash('Pengguna berhasil dihapus.', 'success')
     return redirect(url_for('user'))
 
+# Tampilan admin menu pendaftaran:
+@app.route('/form-admin')
+def registrasi():
+    return render_template('form-admin.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
